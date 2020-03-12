1a6950274a31a5d3598a788149e33232c9274b10
Hrvoje Jurić <hrvoje.juric.13@gmail.com>
2020-03-13 00:37:08 +0100

    Graphics pipeline added.

--- lab2/app.hpp
+++ lab2/app.hpp
@@ -57,6 +57,9 @@ namespace irglab
         std::vector<VkImage> swap_chain_images_ {};
         std::vector<VkImageView> swap_chain_image_views_ {};
 
+        VkRenderPass render_pass_ = VK_NULL_HANDLE;
+        VkPipelineLayout pipeline_layout_ = VK_NULL_HANDLE;
+        VkPipeline graphics_pipeline_ = VK_NULL_HANDLE;
 		
         void init_window()
         {
@@ -83,6 +86,7 @@ namespace irglab
             create_logical_device();
             create_swap_chain();
             create_image_views();
+            create_render_pass();
             create_graphics_pipeline();
         }
 
@@ -98,6 +102,12 @@ namespace irglab
         // ReSharper disable once CppMemberFunctionMayBeConst
         void cleanup()
     	{
+            vkDestroyPipeline(device_, graphics_pipeline_, nullptr);
+        	
+            vkDestroyPipelineLayout(device_, pipeline_layout_, nullptr);
+
+        	vkDestroyRenderPass(device_, render_pass_, nullptr);
+        	
             for (auto image_view : swap_chain_image_views_)
             {
                 vkDestroyImageView(device_, image_view, nullptr);
@@ -152,6 +162,173 @@ namespace irglab
 
             std::cout << "Shader modules created." << std::endl;
         	
+            VkPipelineVertexInputStateCreateInfo vertex_input_info{};
+            vertex_input_info.sType = VK_STRUCTURE_TYPE_PIPELINE_VERTEX_INPUT_STATE_CREATE_INFO;
+            vertex_input_info.vertexBindingDescriptionCount = 0;
+            vertex_input_info.pVertexBindingDescriptions = nullptr; // Optional
+            vertex_input_info.vertexAttributeDescriptionCount = 0;
+            vertex_input_info.pVertexAttributeDescriptions = nullptr; // Optional
+
+            VkPipelineInputAssemblyStateCreateInfo input_assembly{};
+            input_assembly.sType = VK_STRUCTURE_TYPE_PIPELINE_INPUT_ASSEMBLY_STATE_CREATE_INFO;
+            input_assembly.topology = VK_PRIMITIVE_TOPOLOGY_TRIANGLE_LIST;
+            input_assembly.primitiveRestartEnable = VK_FALSE;
+
+            VkViewport viewport{};
+            viewport.x = 0.0f;
+            viewport.y = 0.0f;
+            viewport.width = static_cast<float>(swap_chain_extent_.width);
+            viewport.height = static_cast<float>(swap_chain_extent_.height);
+            viewport.minDepth = 0.0f;
+            viewport.maxDepth = 1.0f;
+
+            VkRect2D scissor{};
+            scissor.offset = { 0, 0 };
+            scissor.extent = swap_chain_extent_;
+
+            VkPipelineViewportStateCreateInfo viewport_state{};
+            viewport_state.sType = VK_STRUCTURE_TYPE_PIPELINE_VIEWPORT_STATE_CREATE_INFO;
+            viewport_state.viewportCount = 1;
+            viewport_state.pViewports = &viewport;
+            viewport_state.scissorCount = 1;
+            viewport_state.pScissors = &scissor;
+
+            VkPipelineRasterizationStateCreateInfo rasterizer {};
+            rasterizer.sType = VK_STRUCTURE_TYPE_PIPELINE_RASTERIZATION_STATE_CREATE_INFO;
+            rasterizer.depthClampEnable = VK_FALSE;
+            rasterizer.rasterizerDiscardEnable = VK_FALSE;
+            rasterizer.polygonMode = VK_POLYGON_MODE_FILL;
+            rasterizer.lineWidth = 1.0f;
+            rasterizer.cullMode = VK_CULL_MODE_BACK_BIT;
+            rasterizer.frontFace = VK_FRONT_FACE_CLOCKWISE;
+            rasterizer.depthBiasEnable = VK_FALSE;
+            rasterizer.depthBiasConstantFactor = 0.0f; // Optional
+            rasterizer.depthBiasClamp = 0.0f; // Optional
+            rasterizer.depthBiasSlopeFactor = 0.0f; // Optional
+
+            VkPipelineMultisampleStateCreateInfo multisampling{};
+            multisampling.sType = VK_STRUCTURE_TYPE_PIPELINE_MULTISAMPLE_STATE_CREATE_INFO;
+            multisampling.sampleShadingEnable = VK_FALSE;
+            multisampling.rasterizationSamples = VK_SAMPLE_COUNT_1_BIT;
+            multisampling.minSampleShading = 1.0f; // Optional
+            multisampling.pSampleMask = nullptr; // Optional
+            multisampling.alphaToCoverageEnable = VK_FALSE; // Optional
+            multisampling.alphaToOneEnable = VK_FALSE; // Optional
+
+            VkPipelineColorBlendAttachmentState color_blend_attachment{};
+            color_blend_attachment.colorWriteMask = VK_COLOR_COMPONENT_R_BIT
+        		| VK_COLOR_COMPONENT_G_BIT
+        		| VK_COLOR_COMPONENT_B_BIT
+        		| VK_COLOR_COMPONENT_A_BIT;
+            color_blend_attachment.blendEnable = VK_FALSE;
+            color_blend_attachment.srcColorBlendFactor = VK_BLEND_FACTOR_ONE; // Optional
+            color_blend_attachment.dstColorBlendFactor = VK_BLEND_FACTOR_ZERO; // Optional
+            color_blend_attachment.colorBlendOp = VK_BLEND_OP_ADD; // Optional
+            color_blend_attachment.srcAlphaBlendFactor = VK_BLEND_FACTOR_ONE; // Optional
+            color_blend_attachment.dstAlphaBlendFactor = VK_BLEND_FACTOR_ZERO; // Optional
+            color_blend_attachment.alphaBlendOp = VK_BLEND_OP_ADD; // Optional
+
+            VkPipelineColorBlendStateCreateInfo color_blending = {};
+            color_blending.sType = VK_STRUCTURE_TYPE_PIPELINE_COLOR_BLEND_STATE_CREATE_INFO;
+            color_blending.logicOpEnable = VK_FALSE;
+            color_blending.logicOp = VK_LOGIC_OP_COPY; // Optional
+            color_blending.attachmentCount = 1;
+            color_blending.pAttachments = &color_blend_attachment;
+            color_blending.blendConstants[0] = 0.0f; // Optional
+            color_blending.blendConstants[1] = 0.0f; // Optional
+            color_blending.blendConstants[2] = 0.0f; // Optional
+            color_blending.blendConstants[3] = 0.0f; // Optional
+
+            VkPipelineLayoutCreateInfo pipeline_layout_info = {};
+            pipeline_layout_info.sType = VK_STRUCTURE_TYPE_PIPELINE_LAYOUT_CREATE_INFO;
+            pipeline_layout_info.setLayoutCount = 0; // Optional
+            pipeline_layout_info.pSetLayouts = nullptr; // Optional
+            pipeline_layout_info.pushConstantRangeCount = 0; // Optional
+            pipeline_layout_info.pPushConstantRanges = nullptr; // Optional
+
+            if (vkCreatePipelineLayout(
+                device_,
+                &pipeline_layout_info,
+                nullptr,
+                &pipeline_layout_) != VK_SUCCESS)
+            {
+                throw std::runtime_error("Failed to create pipeline layout.");
+            }
+
+            std::cout << "Pipeline layout created." << std::endl;
+
+            VkGraphicsPipelineCreateInfo pipeline_info{};
+            pipeline_info.sType = VK_STRUCTURE_TYPE_GRAPHICS_PIPELINE_CREATE_INFO;
+            pipeline_info.stageCount = 2;
+            pipeline_info.pStages = shader_stages.data();
+            pipeline_info.pVertexInputState = &vertex_input_info;
+            pipeline_info.pInputAssemblyState = &input_assembly;
+            pipeline_info.pViewportState = &viewport_state;
+            pipeline_info.pRasterizationState = &rasterizer;
+            pipeline_info.pMultisampleState = &multisampling;
+            pipeline_info.pDepthStencilState = nullptr; // Optional
+            pipeline_info.pColorBlendState = &color_blending;
+            pipeline_info.pDynamicState = nullptr; // Optional
+            pipeline_info.layout = pipeline_layout_;
+            pipeline_info.renderPass = render_pass_;
+            pipeline_info.subpass = 0;
+            pipeline_info.basePipelineHandle = VK_NULL_HANDLE; // Optional
+            pipeline_info.basePipelineIndex = -1; // Optional
+
+            if (vkCreateGraphicsPipelines(
+                device_,
+                VK_NULL_HANDLE, 1,
+                &pipeline_info,
+                nullptr,
+                &graphics_pipeline_) != VK_SUCCESS) 
+            {
+                throw std::runtime_error("failed to create graphics pipeline!");
+            }
+
+            std::cout << "Graphics pipeline created." << std::endl;
+        	
+            vkDestroyShaderModule(device_, fragment_shader_module, nullptr);
+            vkDestroyShaderModule(device_, vertex_shader_module, nullptr);
+        }
+
+		void create_render_pass()
+        {
+            VkAttachmentDescription color_attachment{};
+            color_attachment.format = swap_chain_image_format_;
+            color_attachment.samples = VK_SAMPLE_COUNT_1_BIT;
+            color_attachment.loadOp = VK_ATTACHMENT_LOAD_OP_CLEAR;
+            color_attachment.storeOp = VK_ATTACHMENT_STORE_OP_STORE;
+            color_attachment.stencilLoadOp = VK_ATTACHMENT_LOAD_OP_DONT_CARE;
+            color_attachment.stencilStoreOp = VK_ATTACHMENT_STORE_OP_DONT_CARE;
+        	// Added because validation layers said to add something.
+            color_attachment.finalLayout = VK_IMAGE_LAYOUT_PRESENT_SRC_KHR;
+
+            VkAttachmentReference color_attachment_ref;
+            color_attachment_ref.attachment = 0;
+            color_attachment_ref.layout = VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL;
+
+            VkSubpassDescription subpass{};
+            subpass.pipelineBindPoint = VK_PIPELINE_BIND_POINT_GRAPHICS;
+            subpass.colorAttachmentCount = 1;
+            subpass.pColorAttachments = &color_attachment_ref;
+
+            VkRenderPassCreateInfo render_pass_info{};
+            render_pass_info.sType = VK_STRUCTURE_TYPE_RENDER_PASS_CREATE_INFO;
+            render_pass_info.attachmentCount = 1;
+            render_pass_info.pAttachments = &color_attachment;
+            render_pass_info.subpassCount = 1;
+            render_pass_info.pSubpasses = &subpass;
+
+            if (vkCreateRenderPass(
+                device_,
+                &render_pass_info,
+                nullptr,
+                &render_pass_) != VK_SUCCESS)
+            {
+                throw std::runtime_error("Failed to create render pass.");
+            }
+
+            std::cout << "Render pass created." << std::endl;
         }
 		
 		
@@ -165,12 +342,10 @@ namespace irglab
             app_info.engineVersion = VK_MAKE_VERSION(1, 0, 0);
             app_info.apiVersion = VK_API_VERSION_1_2;
 
-        	
             VkInstanceCreateInfo create_info{};
             create_info.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
             create_info.pApplicationInfo = &app_info;
 
-        	
             auto required_glfw_extensions = get_required_glfw_extension_names();
             if (!glfw_extensions_supported(required_glfw_extensions))
             {
@@ -178,7 +353,6 @@ namespace irglab
             }
             create_info.enabledExtensionCount = static_cast<uint32_t>(required_glfw_extensions.size());
             create_info.ppEnabledExtensionNames = required_glfw_extensions.data();
-
 
             VkDebugUtilsMessengerCreateInfoEXT debug_create_info{};
             populate_debug_messenger_create_info(debug_create_info);
@@ -199,7 +373,6 @@ namespace irglab
             {
                 create_info.enabledLayerCount = 0;
             }
-        	
         	
         	if (vkCreateInstance(&create_info, nullptr, &instance_) != VK_SUCCESS)
         	{
